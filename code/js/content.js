4f5e94ee52a42c51d0211791ce033025ddee0bb8
James Freeman <jfreeman@activecampaign.com>
2017-06-06 08:40:14 -0400

    Open pull request links in new tab

--- code/js/content.js
+++ code/js/content.js
@@ -18,7 +18,7 @@
 	require('./modules/msg').init('ct', handlers);
 
 	$('document').ready(function(){
-		$('body').on('click', 'a.lozenge,a.external-link,.link-title', function(e) {
+		$('body').on('click', 'a.lozenge,a.external-link,.link-title,.pullrequest-link', function(e) {
 			var url = e.currentTarget.href;
 			chrome.runtime.sendMessage({url: url}, function(){});
 			return false;
